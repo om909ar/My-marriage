d61ab946b61e8f89ea111ed8cd3160a2879246cc
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-19 16:55:44 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.querySelectorAll(".stage").forEach(stage => {
 
     stage.addEventListener("click", () => {
 
-        alert(stage.querySelector("span").textContent);
+        stage.style.background = "#3a3a3c";
 
     });
 
